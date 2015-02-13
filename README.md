f14a3985546e49fe049e30a565d139dd29450e43
Gabor Imre <imregabor@gmail.com>
2015-02-13 22:37:12 +0100

    Set up repo

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# serial-rgb-strip
-RGB addressable LED strip driving through serial port
+serial-rgb-strip
+================
+
+RGB addressable LED strip driving through serial port. This is a simple Arduino project.
+
+
+Getting started
+---------------
+
+  - Install Adafruit NeoPixel library from https://github.com/adafruit/Adafruit_NeoPixel
+  - Install CH340 driver when neceassary
+  
+SHA512 sum of the win driver for my iformation  
+
+````
+451a15213c3057a37f83a4250e9d8a18aa0c3df6ee09b05759b767860ef12d19ed263f6a23c7fc6249cb64b8b140766fb3258f24c01faaeca87069f21a52b4b4 *ch341ser.exe
+````
---
+++ serial.ino
@@ -0,0 +1,127 @@
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            6
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      8
+
+// Serial port bitrate
+#define BITRATE   9600
+
+// Setial port state machine
+// We are in empty packets
+#define STATE_IDLE     0
+
+// Something wrong happened, ignore everything until the first newline or ';' character
+#define STATE_ERROR    1
+
+// Question mark found
+#define STATE_QM       2
+
+// We are in receiving data ('!')
+#define STATE_DATA     3
+
+// State of serial receuvubg
+uint8_t serialState;
+
+// Next R/G/B index expected
+uint8_t   rgbindex;
+
+// Next LED address expected
+uint8_t   nextled;
+
+// Next digit index
+uint8_t   nextdigit;
+
+uint8_t[3] rgb;
+
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
+
+  pixels.begin(); // This initializes the NeoPixel library.
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, 0);
+  }
+  pixels.show();
+  
+  serialState = STATE_IDLE;
+  Serial.begin(BITRATE);
+}
+
+void loop() {
+
+  while (Serial.available()) {
+    byte rcv = Serial.read();
+    switch (serialState) {
+      case STATE_IDLE:
+        if (rcv == '?') {
+          serialState = STATE_QM;
+        } else if (rcv == '@') {
+          serialState = STATE_DATA;
+          rgbindex = 0;
+          nextled = 0;
+          nextdigit = 1;
+          rgb[0] = 0;
+          rgb[1] = 0;
+          rgb[2] = 0;
+        } else if (rcv != ' ' && rcv != '\n') {
+          serialState = STATE_ERROR;
+        }
+        break;
+      case STATE_QM:
+        if (rcv == ';') {
+          Serial.print("{ numPixels : ");
+          Serial.print(NUMPIXELS, DEC);
+          Serial.print(", bitRate : ");
+          Serial.print(BITRATE, DEC);
+          Serial.println(" }");
+          serialState = STATE_IDLE;
+        } else {
+          serialState = STATE_ERROR;
+        }
+        break;
+      case STATE_DATA:
+        if (rcv >= '0' && rcv <='9') {
+          rcv -= '0';  
+        } else if (rcv >= 'A' && rcv <='F') {
+          rcv -= 'A';  
+          rcv += 10;
+        } else if (rcv >= 'a' && rcv <='f') {
+          rcv -= 'a';  
+          rcv += 10;
+        } else {
+          serialState = STATE_ERROR;
+          break;
+        }
+        rgb[rgbindex] |= rcv << (4 * nextdigit);
+        
+        break;
+    
+            
+      default: 
+        // Also STATE_ERROR handled here
+        // read and 
+        serialState = STATE_ERROR;
+        if (rcv == '\n' || rcv == ';' || rcv == ' ') {
+          serialState = STATE_IDLE;  
+        } else {
+          Serial.println("?");
+        }
+    }    
+  }
+ 
+  
+}
